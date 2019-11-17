714c7c80f7eced81e408fed2310671772253b89b
Shaquille Galimba <shaqrivergalimba24@gmail.com>
2019-11-17 07:48:30 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -23,3 +23,9 @@ function destructivelyAddElementToEndOfArray(array, element) {
   array.push(element)
   return array
 }
+
+function accessElementInArray(array, index){
+  var = array[]
+
+  return array[2]
+}
