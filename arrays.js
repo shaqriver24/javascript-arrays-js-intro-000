19aab8153b46c3a25978aa4aa54e857d2ab9dea1
Shaquille Galimba <shaqrivergalimba24@gmail.com>
2019-11-17 06:43:17 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -1,8 +1,3 @@
 var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"]
 
-function addElementToEndOfArray(array, element) {
-  var array = []
-  [element, ...array]
 
-  return array
-}
