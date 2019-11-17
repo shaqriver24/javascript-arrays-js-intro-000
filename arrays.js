d505d3591749a743d344014a0bf2d823dcc857c6
Shaquille Galimba <shaqrivergalimba24@gmail.com>
2019-11-17 06:58:14 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -8,6 +8,5 @@ function addElementToBeginningOfArray(array, element) {
 
 function destructivelyAddElementToBeginningOfArray(array, element) {
   var array = [1]
-  array.unshift(element)
-  return array
+  return array.unshift(element)
 }
