5edf4013e66ee417c210702d54a905b0036b59ee
Shaquille Galimba <shaqrivergalimba24@gmail.com>
2019-11-17 07:56:15 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -1,31 +1,5 @@
 var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"];
 
 function addElementToBeginningOfArray(array, element) {
-  var array = ["foo", 1];
-  [element, ...array];
-  return array;
-}
-
-function destructivelyAddElementToBeginningOfArray(array, element) {
-  var array = [1];
-  array.unshift(element)
-  return array
-}
-
-function addElementToEndOfArray(array, element) {
-  var array = [1, "foo"];
-  [...array, element]
-  return array
-}
-
-function destructivelyAddElementToEndOfArray(array, element) {
-  var array = [1]
-  array.push(element)
-  return array
-}
-
-function accessElementInArray(array, index) {
-  var = array[1, 2, 3]
-
-  return array[2]
+  return [element, ...array]
 }
